49a4a1548945f0a6a2a886996891616bfc3eed31
Raffael Schreiber <raffael.schreiber@gmail.com>
2023-07-12 14:01:39 +0200

    added files for uploading stuff

---
+++ upload_cpc.py
@@ -0,0 +1,53 @@
+import datetime
+import ftplib
+
+import numpy as np
+import pandas as pd
+import toml
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+CONF = toml.load("config.toml")
+
+ftp = ftplib.FTP(**CONF["ftp"])
+ftp.cwd(CONF["cpc"]["ftp_folder"])
+cpc_files = ftp.nlst()
+
+# Gets the CPC file from yesterday at 01:00 (UTC offset)
+yesterday = (datetime.datetime.today() - datetime.timedelta(days=1)).strftime("%Y%m%d")
+cpc_yesterday = yesterday[2:4] + yesterday[4:6] + yesterday[6:8] + "01"
+file_yesterday = [s for s in cpc_files if cpc_yesterday in s][0]
+ftp.retrbinary("RETR " + file_yesterday, open(f"csv/{file_yesterday}", "wb").write)
+ftp.quit()
+
+df = pd.read_csv(
+    f"csv/{file_yesterday}",
+    sep=";",
+    encoding="ANSI",
+    skiprows=np.arange(4),
+    header=None,
+)
+df = df.drop(columns=[2, 3])
+df.columns = ["datetime", "particle_number_concentration"]
+df["location"] = "Zuerich [16]"
+df["datetime"] = pd.to_datetime(df["datetime"], format="%d.%m.%Y %H:%M") + pd.Timedelta(
+    hours=-1
+)
+df.set_index("datetime", inplace=True)
+df.drop(df.tail(1).index, inplace=True)
+
+with InfluxDBClient(
+    url="https://influxdb.naneos.ch",
+    token="GZP1oTMT3E3v9USbSzO5IpFZbo8m4dypkKnz5PPaumO-T6bKlGredujj6QiXMRmETSj4WwURR7orQrVvPszwKg==",
+    org="naneos",
+) as client:
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+
+    res = write_api.write(
+        bucket="naneos_test",
+        org="naneos",
+        record=df,
+        data_frame_measurement_name="cpc",
+        data_frame_tag_columns=["location"],
+        data_frame_field_columns=[c for c in df.columns if c not in ["location"]],
+    )
---
+++ upload_smps.py
@@ -0,0 +1,50 @@
+import datetime
+import ftplib
+
+import numpy as np
+import pandas as pd
+import toml
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+CONF = toml.load("config.toml")
+
+ftp = ftplib.FTP(**CONF["ftp"])
+ftp.cwd(CONF["smps"]["ftp_folder"])
+smps_files = ftp.nlst()
+
+yesterday = (datetime.datetime.today() - datetime.timedelta(days=1)).strftime("%Y%m%d")
+file_yesterday = [s for s in smps_files if yesterday in s][0]
+
+ftp.retrbinary("RETR " + file_yesterday, open(f"csv/{file_yesterday}", "wb").write)
+ftp.quit()
+
+df = pd.read_csv(
+    f"csv/{file_yesterday}", sep=",", encoding="utf-8", skiprows=np.arange(52)
+)
+df["datetime"] = pd.to_datetime(
+    df["DateTime Sample Start"], format="%d/%m/%Y %H:%M:%S"
+) + pd.Timedelta(hours=1)
+df.rename(
+    columns={
+        "Test Name": "location",
+        "Total Concentration (#/cm³)": "particle_number_concentration",
+    },
+    inplace=True,
+)
+df.set_index("datetime", inplace=True)
+influx_df = df[CONF["smps"]["cols"]]
+
+with InfluxDBClient(**CONF["influxdb"]["creds"]) as client:
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+
+    res = write_api.write(
+        bucket=CONF["influxdb"]["bucket"],
+        org=CONF["influxdb"]["creds"]["org"],
+        record=influx_df,
+        data_frame_measurement_name=CONF["smps"]["measurement"],
+        data_frame_tag_columns=["location"],
+        data_frame_field_columns=[
+            c for c in influx_df.columns if c not in ["location"]
+        ],
+    )
